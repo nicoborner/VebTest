2e83fb9c0f626316220990211d2878b8742c418a
Nico Borner <48905028+nicoborner@users.noreply.github.com>
2023-09-27 11:33:42 +0200

    Add files via upload

---
+++ LicTest.ps1
@@ -0,0 +1,3 @@
+﻿Install-Module Microsoft.Graph -Scope CurrentUser -Confirm:$false
+Connect-Graph -Scopes Organization.Read.All
+Get-MgSubscribedSku | Select -Property SkuPartNumber, ConsumedUnits -ExpandProperty PrepaidUnits | Where-Object {$_.SkuPartNumber -like "WIN10*"} | Format-List
